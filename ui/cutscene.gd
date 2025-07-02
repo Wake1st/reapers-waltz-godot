3b77be08f70967c1012846e16ed5872cb6932796
Wake1st <joeltwakefield@gmail.com>
2025-07-01 21:11:55 -0600

    improved cutscene

--- ui/cutscene.gd
+++ ui/cutscene.gd
@@ -21,7 +21,8 @@ const FADE_OUT_DURATION: float = 3.6
 
 @onready var shot: TextureRect = $TextureRect
 @onready var blank: ColorRect = $ColorRect
-@onready var audio: AudioStreamPlayer2D = $AudioStreamPlayer2D
+@onready var deathAudio: AudioStreamPlayer2D = $Death
+@onready var music: AudioStreamPlayer2D = $Music
 @onready var timer: Timer = $Timer
 
 var stage: Stage
@@ -35,17 +36,26 @@ func start() -> void:
 func reset() -> void:
 	stage = Stage.FADE_IN
 	timer.stop()
-	blank.visible = false
 	blank.color.a = 1.0
 	shot.size = Vector2(1400, 840)
 	shot.position = Vector2.ZERO
+	music.volume_db = -18
 
+
+func _ready() -> void:
+	reset()
 
 func _fade_in() -> void:
 	stage = Stage.FADE_IN
+	music.play()
+	
 	tween = create_tween()
+	tween.set_parallel(true)
 	tween.tween_property(blank, "color:a", 0.0, FADE_IN_DURATION
 		).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_SINE)
+	tween.tween_property(music, "volume_db", 12, FADE_IN_DURATION
+		).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_SINE)
+	tween.set_parallel(false)
 	tween.tween_callback(_stage_finished)
 
 func _room() -> void:
@@ -56,6 +66,8 @@ func _room() -> void:
 		).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_QUAD)
 	tween.tween_property(shot, "position", FINAL_POSITION, ROOM_DURATION
 		).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_QUAD)
+	tween.tween_property(music, "volume_db", 22, FADE_IN_DURATION
+		).set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_CUBIC)
 	tween.set_parallel(false)
 	tween.tween_callback(_stage_finished)
 
@@ -63,7 +75,8 @@ func _black() -> void:
 	stage = Stage.BLANK
 	blank.color.a = 1.0
 	shot.visible = false
-	audio.play()
+	deathAudio.play()
+	music.stop()
 	timer.start(BLANK_DURATION)
 
 func _fade_out() -> void:
